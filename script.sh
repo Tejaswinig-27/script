3fb83fe50542c3634c7eeb85c2be4b8f565df2a4
Tejaswinig-27 <66554551+Tejaswinig-27@users.noreply.github.com>
2023-05-12 14:56:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,9 @@ echo "........start........"
 mkdir tejudev
 cd tejudev
 touch file1 > "this is my first file"
+touch file2 > "this is my second file"
+ls
+cat file1
+cat file2
 echo "display file"
 echo ".......end......."
